430c32f8aaea390d2d4618ce67604b2d0f182765
Jean carlos <piguavejc@gmail.com>
2024-09-08 13:48:46 -0500

    Refactor cron job to sync email sent using axios

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-import cron from 'node-cron';
 
-cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
+import 'dotenv/config';
+import { schedule } from 'node-cron';
+import axios from '../node_modules/axios/index';
+
+schedule(`*/1 * * * *`, async () => {
+  const API_URL = process.env.API_URL;
+  try {
+   const result =  await axios.post(`${API_URL}`,
+      {
+        "operationName": null,
+        "variables": {},
+        "query": "mutation {\n  syncEmailSent {\n    message\n  }\n}\n"
+      });
+    console.log(result.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error.response?.data);
+      return 
+    }
+    console.error(error);
+  }
+
+  console.log('Cron job ran');
+
 });
