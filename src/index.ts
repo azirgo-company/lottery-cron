ecbba42f07e0ce76f2a16da21bd93448eff85f03
Jean carlos <piguavejc@gmail.com>
2024-09-13 18:34:33 -0500

    Refactor cron job to sync invoices using axios

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import 'dotenv/config';
 import { schedule } from 'node-cron';
 import axios from '../node_modules/axios/index';
 
+const API_URL = process.env.API_URL;
 schedule(`*/1 * * * *`, async () => {
-  const API_URL = process.env.API_URL;
   try {
    const result =  await axios.post(`${API_URL}`,
       {
@@ -23,4 +23,22 @@ schedule(`*/1 * * * *`, async () => {
 
   console.log('Cron job ran');
 
+});
+
+schedule(`*/1 * * * *`, async () => { 
+  try {
+    const result = await axios.post(`${API_URL}`, {
+      "operationName": null,
+      "variables": {},
+      "query": "mutation {\n  syncInvoices {\n    message\n  }\n}\n"
+    })
+    console.log(result.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error.response?.data);
+      return 
+    }
+    console.error(error);
+  }
+  console.log('Cron invoices job ran');
 });
